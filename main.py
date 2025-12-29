59c90c472feff1fa3cfadc591781bfbe7631cb3b
ThomasB <thomasbaurschmid@gmail.com>
2025-12-29 18:49:41 +0100

    split

--- main.py
+++ main.py
@@ -65,16 +65,28 @@ def go(config: DictConfig):
                 )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                    os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                    "main",
+                    parameters={
+                        "csv": "clean_sample.csv:latest",
+                        "ref": "clean_sample.csv:reference",
+                        "kl_threshold": config['data_check']['kl_threshold'],
+                        "min_price": config['etl']['min_price'],
+                        "max_price": config['etl']['max_price']
+                    },
+                )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                parameters={
+                    "input": "clean_sample.csv:latest", 
+                    "test_size": config['modeling']['test_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by']
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
